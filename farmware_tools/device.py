21a1dc70a7509be0274119f35230783ce9fc028c
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-05-15 01:05:21 -0700

    add get bot info functions

--- farmware_tools/device.py
+++ farmware_tools/device.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 import os
 import sys
 import json
+from functools import wraps
 import requests
 
 def _color(string):
@@ -22,41 +23,85 @@ def _on_error():
     sys.exit(1)
     return
 
-def _check_and_format(command):
+def _check_celery_script(command):
     try:
         kind = command['kind']
         args = command['args']
     except (KeyError, TypeError):
-        to_print = command
-        _error('celery script', command)
+        _cs_error('celery script', command)
         _on_error()
     else:
-        to_print = "{{'kind': '{magenta}{kind}{reset}', " \
-        "'args': {cyan}{args}{reset}}}".format(
-            magenta=MAGENTA, cyan=CYAN, reset=RESET, kind=kind, args=args)
+        return kind, args
+
+def _colorize_celery_script(kind, args):
+    to_print = "{{'kind': '{magenta}{kind}{reset}', " \
+    "'args': {cyan}{args}{reset}}}".format(
+        magenta=MAGENTA, cyan=CYAN, reset=RESET, kind=kind, args=args)
     return to_print
 
 def rpc_wrapper(command, rpc_id=''):
     'Wrap a command in `rpc_request`.'
     return {'kind': 'rpc_request', 'args': {'label': rpc_id}, 'body': [command]}
 
-def send_celery_script(command):
-    'Send a celery script command.'
-    to_print = _check_and_format(command)
+def post(endpoint, payload):
+    'Post a payload to the device Farmware API.'
     try:
         url = os.environ['FARMWARE_URL']
         token = os.environ['FARMWARE_TOKEN']
     except KeyError:
-        print(to_print)
+        return
     else:
         response = requests.post(
-            url + 'api/v1/celery_script',
+            url + 'api/v1/' + endpoint,
             headers={'Authorization': 'Bearer ' + token,
                      'content-type': 'application/json'},
-            data=json.dumps(command))
+            data=json.dumps(payload))
         if response.status_code != 200:
-            log('Invalid celery script `{}`'.format(command), 'error')
+            log('Invalid {} request `{}` ({})'.format(
+                endpoint, payload, response.status_code), 'error')
             _on_error()
+        return response
+
+def get(endpoint):
+    'Get info from the device Farmware API.'
+    try:
+        url = os.environ['FARMWARE_URL']
+        token = os.environ['FARMWARE_TOKEN']
+    except KeyError:
+        return
+    else:
+        response = requests.get(
+            url + 'api/v1/' + endpoint,
+            headers={'Authorization': 'Bearer ' + token,
+                     'content-type': 'application/json'})
+        if response.status_code != 200:
+            log('Invalid {} request ({})'.format(
+                endpoint, response.status_code), 'error')
+            _on_error()
+        return response
+
+def get_bot_state():
+    'Get the device state.'
+    bot_state = get('bot/state')
+    if bot_state is None:
+        _error('Device info could not be retrieved.')
+        _on_error()
+    else:
+        return bot_state.json()
+
+def _send(function):
+    @wraps(function)
+    def wrapper(*args, **kwargs):
+        'Send Celery Script to the device.'
+        return send_celery_script(function(*args, **kwargs))
+    return wrapper
+
+def send_celery_script(command):
+    'Send a celery script command.'
+    kind, args = _check_celery_script(command)
+    response = post('celery_script', command)
+    if response is None:
+        print(_colorize_celery_script(kind, args))
     return command
 
 def log(message, message_type='info'):
@@ -67,7 +112,15 @@ def _assemble(kind, args):
     'Assemble a celery script command.'
     return {'kind': kind, 'args': args}
 
-def _error(kind, arg):
+def _error(error):
+    try:
+        os.environ['FARMWARE_URL']
+    except KeyError:
+        print(RED + error + RESET)
+    else:
+        log(error, 'error')
+
+def _cs_error(kind, arg):
     try:
         os.environ['FARMWARE_URL']
     except KeyError:
@@ -80,7 +133,7 @@ def _check_arg(kind, arg, accepted):
     'Error and exit for invalid command arguments.'
     arg_ok = True
     if arg not in accepted:
-        _error(kind, arg)
+        _cs_error(kind, arg)
         _on_error()
         arg_ok = False
     return arg_ok
@@ -99,97 +152,98 @@ def _check_coordinate(coordinate):
     except (KeyError, TypeError):
         coordinate_ok = False
     if not coordinate_ok:
-        _error('coordinate', coordinate)
+        _cs_error('coordinate', coordinate)
         _on_error()
     return coordinate_ok
 
+@_send
 def send_message(message, message_type):
     'Send command: send_message'
     kind = 'send_message'
     allowed_types = ['success', 'busy', 'warn', 'error', 'info', 'fun', 'debug']
     args_ok = _check_arg(kind, message_type, allowed_types)
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'message': message,
-                             'message_type': message_type}))
+        return _assemble(kind, {'message': message,
+                                'message_type': message_type})
 
+@_send
 def calibrate(axis):
     'Send command: calibrate'
     kind = 'calibrate'
     args_ok = _check_arg(kind, axis, ['x', 'y', 'z', 'all'])
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'axis': axis}))
+        return _assemble(kind, {'axis': axis})
 
+@_send
 def check_updates(package):
     'Send command: check_updates'
     kind = 'check_updates'
     args_ok = _check_arg(kind, package,
                          ['farmbot_os', 'arduino_firmware', 'farmware'])
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'package': package}))
+        return _assemble(kind, {'package': package})
 
+@_send
 def emergency_lock():
     'Send command: emergency_lock'
     kind = 'emergency_lock'
-    return send_celery_script(
-        _assemble(kind, {}))
+    return _assemble(kind, {})
 
+@_send
 def emergency_unlock():
     'Send command: emergency_unlock'
     kind = 'emergency_unlock'
-    return send_celery_script(
-        _assemble(kind, {}))
+    return _assemble(kind, {})
 
+@_send
 def execute(sequence_id):
     'Send command: execute'
     kind = 'execute'
-    return send_celery_script(
-        _assemble(kind, {'sequence_id': sequence_id}))
+    return _assemble(kind, {'sequence_id': sequence_id})
 
+@_send
 def execute_script(label):
     'Send command: execute_script'
     kind = 'execute_script'
-    return send_celery_script(
-        _assemble(kind, {'label': label}))
+    return _assemble(kind, {'label': label})
 
+@_send
 def factory_reset(package):
     'Send command: factory_reset'
     kind = 'factory_reset'
     args_ok = _check_arg(kind, package, ['farmbot_os', 'arduino_firmware'])
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'package': package}))
+        return _assemble(kind, {'package': package})
 
+@_send
 def find_home(axis):
     'Send command: find_home'
     kind = 'find_home'
     args_ok = _check_arg(kind, axis, ['x', 'y', 'z', 'all'])
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'axis': axis}))
+        return _assemble(kind, {'axis': axis})
 
+@_send
 def home(axis):
     'Send command: home'
     kind = 'home'
     args_ok = _check_arg(kind, axis, ['x', 'y', 'z', 'all'])
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'axis': axis}))
+        return _assemble(kind, {'axis': axis})
 
+@_send
 def install_farmware(url):
     'Send command: install_farmware'
     kind = 'install_farmware'
-    return send_celery_script(
-        _assemble(kind, {'url': url}))
+    return _assemble(kind, {'url': url})
 
+@_send
 def install_first_party_farmware():
     'Send command: install_first_party_farmware'
     kind = 'install_first_party_farmware'
-    return send_celery_script(
-        _assemble(kind, {}))
+    return _assemble(kind, {})
 
+@_send
 def move_absolute(location, speed, offset):
     'Send command: move_absolute'
     kind = 'move_absolute'
@@ -197,150 +251,157 @@ def move_absolute(location, speed, offset):
     args_ok = _check_coordinate(offset)
     args_ok = _check_arg(kind, speed, range(1, 101))
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'location': location,
-                             'speed': speed,
-                             'offset': offset}))
+        return _assemble(kind, {'location': location,
+                                'speed': speed,
+                                'offset': offset})
 
+@_send
 def move_relative(x, y, z, speed):
     'Send command: move_relative'
     kind = 'move_relative'
     args_ok = _check_arg(kind, speed, range(1, 101))
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'x': x,
-                             'y': y,
-                             'z': z,
-                             'speed': speed}))
+        return _assemble(kind, {'x': x,
+                                'y': y,
+                                'z': z,
+                                'speed': speed})
 
+@_send
 def power_off():
     'Send command: power_off'
     kind = 'power_off'
-    return send_celery_script(
-        _assemble(kind, {}))
+    return _assemble(kind, {})
 
+@_send
 def read_pin(pin_number, label, pin_mode):
     'Send command: read_pin'
     kind = 'read_pin'
     args_ok = _check_arg(kind, pin_number, range(0, 70))
     args_ok = _check_arg(kind, pin_mode, [0, 1])
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'pin_number': pin_number,
-                             'label': label,
-                             'pin_mode': pin_mode}))
+        return _assemble(kind, {'pin_number': pin_number,
+                                'label': label,
+                                'pin_mode': pin_mode})
 
+@_send
 def read_status():
     'Send command: read_status'
     kind = 'read_status'
-    return send_celery_script(
-        _assemble(kind, {}))
+    return _assemble(kind, {})
 
+@_send
 def reboot():
     'Send command: reboot'
     kind = 'reboot'
-    return send_celery_script(
-        _assemble(kind, {}))
+    return _assemble(kind, {})
 
+@_send
 def register_gpio(sequence_id, pin_number):
     'Send command: register_gpio'
     kind = 'register_gpio'
     args_ok = _check_arg(kind, pin_number, range(1, 30))
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'sequence_id': sequence_id,
-                             'pin_number': pin_number}))
+        return _assemble(kind, {'sequence_id': sequence_id,
+                                'pin_number': pin_number})
 
+@_send
 def remove_farmware(package):
     'Send command: remove_farmware'
     kind = 'remove_farmware'
-    return send_celery_script(
-        _assemble(kind, {'package': package}))
+    return _assemble(kind, {'package': package})
 
+@_send
 def set_pin_io_mode(pin_io_mode, pin_number):
     'Send command: set_pin_io_mode'
     kind = 'set_pin_io_mode'
     args_ok = _check_arg(kind, pin_io_mode, [0, 1, 2])
     args_ok = _check_arg(kind, pin_number, range(0, 70))
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'pin_io_mode': pin_io_mode,
-                             'pin_number': pin_number}))
+        return _assemble(kind, {'pin_io_mode': pin_io_mode,
+                                'pin_number': pin_number})
 
+@_send
 def set_servo_angle(pin_number, pin_value):
     'Send command: set_servo_angle'
     kind = 'set_servo_angle'
     args_ok = _check_arg(kind, pin_number, range(4, 6))
     args_ok = _check_arg(kind, pin_value, range(0, 360))
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'pin_number': pin_number,
-                             'pin_value': pin_value}))
+        return _assemble(kind, {'pin_number': pin_number,
+                                'pin_value': pin_value})
 
+@_send
 def sync():
     'Send command: sync'
     kind = 'sync'
-    return send_celery_script(
-        _assemble(kind, {}))
+    return _assemble(kind, {})
 
+@_send
 def take_photo():
     'Send command: take_photo'
     kind = 'take_photo'
-    return send_celery_script(
-        _assemble(kind, {}))
+    return _assemble(kind, {})
 
+@_send
 def toggle_pin(pin_number):
     'Send command: toggle_pin'
     kind = 'toggle_pin'
     args_ok = _check_arg(kind, pin_number, range(0, 70))
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'pin_number': pin_number}))
+        return _assemble(kind, {'pin_number': pin_number})
 
+@_send
 def unregister_gpio(pin_number):
     'Send command: unregister_gpio'
     kind = 'unregister_gpio'
     args_ok = _check_arg(kind, pin_number, range(0, 70))
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'pin_number': pin_number}))
+        return _assemble(kind, {'pin_number': pin_number})
 
+@_send
 def update_farmware(package):
     'Send command: update_farmware'
     kind = 'update_farmware'
-    return send_celery_script(
-        _assemble(kind, {'package': package}))
+    return _assemble(kind, {'package': package})
 
+@_send
 def wait(milliseconds):
     'Send command: wait'
     kind = 'wait'
-    return send_celery_script(
-        _assemble(kind, {'milliseconds': milliseconds}))
+    return _assemble(kind, {'milliseconds': milliseconds})
 
+@_send
 def write_pin(pin_number, pin_value, pin_mode):
     'Send command: write_pin'
     kind = 'write_pin'
     args_ok = _check_arg(kind, pin_number, range(0, 70))
     args_ok = _check_arg(kind, pin_mode, [0, 1])
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'pin_number': pin_number,
-                             'pin_value': pin_value,
-                             'pin_mode': pin_mode}))
+        return _assemble(kind, {'pin_number': pin_number,
+                                'pin_value': pin_value,
+                                'pin_mode': pin_mode})
 
+@_send
 def zero(axis):
     'Send command: zero'
     kind = 'zero'
     args_ok = _check_arg(kind, axis, ['x', 'y', 'z', 'all'])
     if args_ok:
-        return send_celery_script(
-            _assemble(kind, {'axis': axis}))
+        return _assemble(kind, {'axis': axis})
+
+def get_current_position():
+    'Get the current position.'
+    return get_bot_state()['location_data']['position']
+
+def get_pin_value(pin_number):
+    'Get a value from a pin.'
+    return get_bot_state()['pins'][str(pin_number)]['value']
 
 if __name__ == '__main__':
     send_celery_script({'kind': 'read_status', 'args': {}})
     log('Hello World!')
     send_message('Hello World!', 'success')
-    calibrate('x')
+    # calibrate('x')
     check_updates('farmbot_os')
     emergency_lock()
     emergency_unlock()
@@ -372,3 +433,14 @@ if __name__ == '__main__':
     wait(100)
     write_pin(1, 1, 0)
     zero('z')
+    # preferred method for logging position
+    log('At position: ({{x}}, {{y}}, {{z}})')
+    # get position for calculations
+    POSITION = get_current_position()
+    log('At position: ({}, {}, {})'.format(
+        POSITION['x'], POSITION['y'], POSITION['z']))
+    # preferred method for logging pin value
+    log('pin 13 value: {{pin13}}')
+    # get pin value for calculations
+    VALUE = get_pin_value(13)
+    log('pin 13 value: {}'.format(VALUE))
