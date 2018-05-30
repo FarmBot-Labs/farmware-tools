e1ce8c5462d6653177a3aaf6d95f57acaf4e7e94
gabrielburnworth <gabrielburnworth@users.noreply.github.com>
2018-05-30 16:18:44 -0700

    test updates

--- tests/farmware_tools_tests.py
+++ tests/farmware_tools_tests.py
@@ -4,6 +4,7 @@
 
 from __future__ import print_function
 import os
+import sys
 import uuid
 import json
 import time
@@ -74,6 +75,7 @@ def _new_uuid(label=''):
     return str(uuid.uuid4())[:-len(label)] + label
 
 LOG_FW_CMD_CONFIG_KEY = 'firmware_output_log'
+TIMEOUT_SECONDS = 10
 
 class Tester(object):
     'Test device commands.'
@@ -87,7 +89,7 @@ class Tester(object):
         self.all_client_comms = {}  # {'uuid': 'kind'}
         self.elapsed = []
         self.logs_string = ''
-        self.verbose = True
+        self.status = None
 
     def setup(self):
         'Pre-test config.'
@@ -95,19 +97,19 @@ class Tester(object):
         print('TEST SETUP:')
         app.put('fbos_config', payload={LOG_FW_CMD_CONFIG_KEY: True},
                 get_info=app_login)
-        self.wait_for_log(LOG_FW_CMD_CONFIG_KEY)
+        self.wait_for_log(LOG_FW_CMD_CONFIG_KEY, count_time=False)
         print('-' * 50)
 
-    def tear_down(self):
+    def teardown(self):
         'Post-test config.'
         print('-' * 50)
-        print('TEST TEAR DOWN:')
+        print('TEST TEARDOWN:')
         app.put('fbos_config', payload={LOG_FW_CMD_CONFIG_KEY: False},
                 get_info=app_login)
-        self.wait_for_log(LOG_FW_CMD_CONFIG_KEY)
+        self.wait_for_log(LOG_FW_CMD_CONFIG_KEY, count_time=False)
         print('-' * 50)
 
-    def test(self, command, expected_log=None, rpc_id=None):
+    def test(self, command, expected=None, rpc_id=None):
         'Test a command on the device.'
         if command is not None:
             kind = command['kind']
@@ -115,9 +117,14 @@ class Tester(object):
             rpc_test_id = _new_uuid('test') if rpc_id is None else rpc_id
             send(command, self.login_info, rpc_test_id)
             self.outgoing[rpc_test_id] = {'kind': kind, 'time': time.time()}
-            self.wait_for_response(kind, rpc_test_id)
-            if expected_log is not None:
-                self.wait_for_log(expected_log)
+            self.wait_for_response(rpc_test_id)
+            if expected is not None:
+                if expected.get('log') is not None:
+                    for expected_log in expected['log']:
+                        self.wait_for_log(expected_log)
+                if expected.get('status') is not None:
+                    for status in expected['status']:
+                        self.wait_for_status(status['keys'], status['value'])
         else:
             print('command is {}'.format(command))
 
@@ -135,6 +142,8 @@ class Tester(object):
         subscribe(host, user, password, client_channel, self.add_client_comm)
         log_channel = self._get_channel_name('logs')
         subscribe(host, user, password, log_channel, self.add_log_message)
+        status_channel = self._get_channel_name('status')
+        subscribe(host, user, password, status_channel, self.update_status)
 
     def add_response(self, _client, _userdata, message):
         'Add a response to the list.'
@@ -146,6 +155,13 @@ class Tester(object):
                 if rpc_id != 'ping':
                     self.incoming[rpc_id] = {
                         'status': kind.split('_')[-1], 'time': time.time()}
+                    if kind == 'rpc_error':
+                        try:
+                            error_message = parsed['body'][0]['args']['message']
+                        except KeyError:
+                            pass
+                        else:
+                            self.incoming[rpc_id]['error'] = error_message
 
     def add_log_message(self, _client, _userdata, message):
         'Add log message string to the list.'
@@ -166,58 +182,88 @@ class Tester(object):
             else:
                 self.all_client_comms[rpc_id] = kind
 
-    def wait_for_log(self, string):
-        'Wait for a specific log message string.'
-        begin = time.time()
-        while (time.time() - begin) < 10:
-            if string in self.logs_string:
-                print('{}`{}`{} spotted in logs.'.format(
-                    COLOR.bold, string, COLOR.reset))
-                break
-        else:
-            time_diff = time.time() - begin
-            print('{}TIMEOUT{} waiting for `{}` in logs {:.2f}s'.format(
-                COLOR.red, COLOR.reset, string, time_diff))
-        print()
+    def update_status(self, _client, _userdata, message):
+        'Update last status received.'
+        if 'status' in message.topic:
+            parsed = json.loads(message.payload.decode())
+            self.status = parsed
 
-    def wait_for_response(self, kind, rpc_id):
-        'Wait for the device response.'
-        timeout_seconds = 10
+    def wait_for_log(self, string, count_time=True):
+        'Wait for a specific log message string.'
+        def _result(outcome, begin_time):
+            time_diff = time.time() - begin_time
+            print('{}{}{} {}`{}`{} {:.2f}ms'.format(
+                COLOR.green if outcome == 'ok' else COLOR.red,
+                outcome, COLOR.reset,
+                COLOR.bold, string, COLOR.reset, time_diff * 1000))
+            return time_diff
         begin = time.time()
-        kind = self.outgoing[rpc_id]['kind']
-        print_kind = '' if self.verbose else COLOR.magenta + kind + ' '
-        out = self.outgoing[rpc_id]['time']
-        while (time.time() - begin) < timeout_seconds:
-            if rpc_id in self.incoming:
-                status = self.incoming[rpc_id]['status']
-                _in = self.incoming[rpc_id]['time']
-                time_diff = _in - out
-                color = COLOR.green if status == 'ok' else COLOR.red
-                print('{}{}{}{} {:.2f}s'.format(
-                    print_kind, color, status, COLOR.reset, time_diff))
-                if self.verbose:
-                    print()
+        print('expected logs:'.upper(), end=' ')
+        while (time.time() - begin) < TIMEOUT_SECONDS:
+            if string in self.logs_string:
+                time_diff = _result('ok', begin)
                 break
         else:
-            time_diff = time.time() - out
-            print('{}{}TIMEOUT{} {:.2f}s'.format(
-                print_kind, COLOR.red, COLOR.reset, time_diff))
-            if self.verbose:
-                print()
+            time_diff = _result('timeout', begin)
+        if count_time:
+            self.elapsed.append(time_diff)
+
+    def wait_for_status(self, keys, value):
+        'Wait for a specific status value.'
+        key_string = '.'.join(keys)
+        def _result(outcome, begin_time):
+            time_diff = time.time() - begin_time
+            print('{}{}{} {}{} == {}{} {:.2f}ms'.format(
+                COLOR.green if outcome == 'ok' else COLOR.red,
+                outcome, COLOR.reset,
+                COLOR.bold, key_string, repr(value),
+                COLOR.reset, time_diff * 1000))
+            return time_diff
+        def _extract_value(status):
+            unwrapped = status
+            for key in keys:
+                unwrapped = unwrapped.get(key)
+            return unwrapped
+        begin = time.time()
+        print('expected status:'.upper(), end=' ')
+        while (time.time() - begin) < TIMEOUT_SECONDS:
+            if _extract_value(self.status) == value:
+                time_diff = _result('ok', begin)
+                break
+        else:
+            time_diff = _result('timeout', begin)
+        self.elapsed.append(time_diff)
+
+    def wait_for_response(self, rpc_id):
+        'Wait for the device response.'
+        def _result(outcome, begin_time):
+            time_diff = time.time() - begin_time
+            print('{}{}{} {:.2f}s'.format(
+                COLOR.green if outcome == 'ok' else COLOR.red,
+                outcome, COLOR.reset, time_diff))
+            return time_diff
+        begin = time.time()
+        print('rpc status:'.upper(), end=' ')
+        out = self.outgoing[rpc_id]['time']
+        while (time.time() - begin) < TIMEOUT_SECONDS:
+            if rpc_id in self.incoming:
+                rpc_response = self.incoming[rpc_id]
+                status = rpc_response['status']
+                time_diff = _result(status, out)
+                if rpc_response.get('error') is not None:
+                    print(COLOR.error(rpc_response['error']))
+                break
+        else:
+            time_diff = _result('timeout', out)
         self.elapsed.append(time_diff)
 
     def print_elapsed_time(self):
         'Calculate total test time.'
         print('Total time elapsed: {:.2f}s'.format(sum(self.elapsed)))
 
-    def print_summary(self):
-        'Print test summary data table.'
-        table = '{:<40}{:<30}{:<10}{:>12}'
-        titles = ['uuid', 'kind', 'status', 'elapsed (ms)']
-        print(table.format(*titles))
-        underline = table.format(*['-' * 7 for _ in titles])
-        print(underline)
+    def _prepare_summary(self):
         summary = {}
+        # Add records for each RPC response
         for rpc_uuid, in_data in self.incoming.items():
             data = {}
             data['uuid'] = rpc_uuid
@@ -235,15 +281,33 @@ class Tester(object):
                 data['elapsed'] = str(int(round(elapsed_time_float)))
             data['status'] = in_data['status']
             summary[self.incoming[rpc_uuid]['time']] = data
-        in_times = sorted([rpc_in['time'] for rpc_in in self.incoming.values()])
-        for in_time in in_times:
-            data = summary[in_time]
+        # Add records for missing RPC responses
+        for rpc_uuid, out_data in self.outgoing.items():
+            if rpc_uuid not in self.incoming.keys():
+                data = {}
+                data['uuid'] = rpc_uuid
+                data['kind'] = out_data['kind']
+                data['status'] = 'timeout'
+                data['elapsed'] = TIMEOUT_SECONDS * 1000
+                summary[out_data['time']] = data
+        return summary
+
+    def print_summary(self):
+        'Print test summary data table.'
+        table = '{:<40}{:<30}{:<10}{:>12}'
+        titles = ['uuid', 'kind', 'status', 'elapsed (ms)']
+        print(table.format(*titles))
+        underline = table.format(*['-' * 7 for _ in titles])
+        print(underline)
+        summary = self._prepare_summary()
+        for _, data in sorted(summary.items()):
             print(table.format(
                 data['uuid'], data['kind'], data['status'], data['elapsed']))
         print(underline)
         all_ok = all(d['status'] == 'ok' for u, d in self.incoming.items())
         print(table.format(
-            'count:', len(self.incoming),
+            'received/sent:',
+            '{}/{}'.format(len(self.incoming), len(self.outgoing)),
             'ok' if all_ok else 'error',
             int(round(sum(self.elapsed) * 1000))))
         print()
@@ -257,15 +321,21 @@ if __name__ == '__main__':
             'token': mqtt_login['token'],
             'url': mqtt_login['url'],
             'verbose': True}
+    def run_test(category):
+        'Prompt for test category run.'
+        run = INPUT('Run {} tests? (Y/n) '.format(category)) or 'y'
+        if run.lower() == 'q':
+            sys.exit(0)
+        if run.lower() == 'y':
+            return True
 
     # Device tests
-    RUN = INPUT('Run device tests? (Y/n) ') or 'y'
-    if RUN.lower() == 'y':
+    if run_test('device'):
         if not LOGIN_INFO:
             TEST = Tester()
             LOGIN_INFO = True
 
-        COORDINATE = device.assemble_coordinate(1, 1, 1)
+        COORDINATE = device.assemble_coordinate(1, 0, 1)
         OFFSET = device.assemble_coordinate(0, 0, 0)
         URL = 'https://raw.githubusercontent.com/FarmBot-Labs/farmware_manifests/' \
             'master/packages/take-photo/manifest.json'
@@ -276,27 +346,32 @@ if __name__ == '__main__':
              'kwargs': {'message': 'hi', 'rpc_id': 'abcd'}},
             {'command': device.check_updates,
              'kwargs': {'package': 'farmbot_os'}},
-            {'command': device.emergency_lock, 'kwargs': {}},
+            {'command': device.emergency_lock, 'kwargs': {},
+             'expected': {'status': [{
+                 'keys': ['informational_settings', 'locked'],
+                 'value': True}]}},
             {'command': device.emergency_unlock, 'kwargs': {},
-             'expected': {'log': 'F09'}},
+             'expected': {'log': ['F09'], 'status': [{
+                 'keys': ['informational_settings', 'locked'],
+                 'value': False}]}},
             {'command': device.execute, 'kwargs': {'sequence_id': SEQUENCE}},
             {'command': device.execute_script,
              'kwargs': {'label': 'take-photo'}},
-            {'command': device.find_home, 'kwargs': {'axis': 'x'},
-             'expected': {'log': 'F11'}},
+            {'command': device.find_home, 'kwargs': {'axis': 'y'},
+             'expected': {'log': ['F12']}},
             {'command': device.home, 'kwargs': {'axis': 'z'},
-             'expected': {'log': 'G00 Z0'}},
+             'expected': {'log': ['G00 Z0']}},
             {'command': device.install_farmware, 'kwargs': {'url': URL}},
             {'command': device.install_first_party_farmware, 'kwargs': {}},
             {'command': device.move_absolute,
              'kwargs': {'location': COORDINATE, 'speed': 100, 'offset': OFFSET},
-             'expected': {'log': 'G00 X1.0 Y1.0 Z1.0'}},
+             'expected': {'log': ['G00 X1.0 Y0.0 Z1.0']}},
             {'command': device.move_relative,
-             'kwargs': {'x': 0, 'y': 0, 'z': 0, 'speed': 100},
-             'expected': {'log': 'G00 X1.0 Y1.0 Z0.0'}},
+             'kwargs': {'x': -1, 'y': 0, 'z': 0, 'speed': 100},
+             'expected': {'log': ['G00 X0.0 Y0.0 Z0.0']}},
             {'command': device.read_pin,
              'kwargs': {'pin_number': 1, 'label': 'label', 'pin_mode': 0},
-             'expected': {'log': 'F42 P1 M0'}},
+             'expected': {'log': ['F42 P1 M0']}},
             {'command': device.read_status, 'kwargs': {}},
             {'command': device.register_gpio,
              'kwargs': {'pin_number': 1, 'sequence_id': SEQUENCE}},
@@ -304,23 +379,31 @@ if __name__ == '__main__':
              'kwargs': {'package': 'farmware'}},
             {'command': device.set_pin_io_mode,
              'kwargs': {'pin_io_mode': 0, 'pin_number': 47},
-             'expected': {'log': 'F43 P47 M0'}},
+             'expected': {'log': ['F43 P47 M0']}},
             {'command': device.set_servo_angle,
              'kwargs': {'pin_number': 4, 'pin_value': 1},
-             'expected': {'log': 'F61 P4 V1'}},
-            {'command': device.sync, 'kwargs': {}},
+             'expected': {'log': ['F61 P4 V1']}},
+            {'command': device.sync, 'kwargs': {},
+             'expected': {'status': [{
+                 'keys': ['informational_settings', 'sync_status'],
+                 'value': 'synced'}]}},
             {'command': device.take_photo, 'kwargs': {}},
             {'command': device.toggle_pin, 'kwargs': {'pin_number': 1},
-             'expected': {'log': 'F41 P1 V'}},
+             'expected': {'log': ['F41 P1 V']}},
             {'command': device.unregister_gpio, 'kwargs': {'pin_number': 1}},
             {'command': device.update_farmware,
              'kwargs': {'package': 'take-photo'}},
             {'command': device.wait, 'kwargs': {'milliseconds': 100}},
             {'command': device.write_pin,
              'kwargs': {'pin_number': 1, 'pin_value': 1, 'pin_mode': 0},
-             'expected': {'log': 'F41 P1 V1 M0'}},
+             'expected': {'log': ['F41 P1 V1 M0'], 'status': [{
+                 'keys': ['pins', '1', 'value'],
+                 'value': 1}]}},
             {'command': device.zero, 'kwargs': {'axis': 'y'},
-             'expected': {'log': 'F84 Y1'}},
+             'expected': {'log': ['F84 Y1'], 'status': [
+                 {'keys': ['location_data', 'position', 'y'], 'value': 0},
+                 {'keys': ['location_data', 'scaled_encoders', 'y'], 'value': 0}
+                 ]}},
         ]
         TEST.setup()
         for test in TESTS:
@@ -328,21 +411,17 @@ if __name__ == '__main__':
                 _rpc_id = test['kwargs'].pop('rpc_id')
             except KeyError:
                 _rpc_id = None
-            try:
-                _expected_log = test['expected']['log']
-            except KeyError:
-                _expected_log = None
+            print()
             TEST.test(test['command'](**test['kwargs']),
-                      rpc_id=_rpc_id, expected_log=_expected_log)
+                      rpc_id=_rpc_id, expected=test.get('expected'))
         print('=' * 20)
         TEST.print_elapsed_time()
         print()
-        TEST.tear_down()
+        TEST.teardown()
         TEST.print_summary()
 
     # App tests
-    RUN = INPUT('Run app tests? (Y/n) ') or 'y'
-    if RUN.lower() == 'y':
+    if run_test('app'):
         if not LOGIN_INFO:
             TEST = Tester()
             LOGIN_INFO = True
@@ -401,8 +480,7 @@ if __name__ == '__main__':
         assert value == expected
         print('`{}` value {} == {}'.format(key, value, expected))
 
-    RUN = INPUT('Run other tests? (Y/n) ') or 'y'
-    if RUN.lower() == 'y':
+    if run_test('other'):
         os.environ['farmware_name_int_input'] = '10'
         os.environ['farmware_name_str_input'] = 'ten'
         _print_header('farmware_tools.get_config_value():')
